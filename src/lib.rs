aea42a8922a4c1a96d56b9a66395d92a5b4b8ef0
aki-cat <wakiramizutani@gmail.com>
2025-08-27 22:59:33 -0300

    Get directory names from args

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,16 @@ use std::path;
 
 use image::ExtendedColorType;
 
-pub fn convert(file_path: &path::Path) -> anyhow::Result<()> {
+pub fn convert(file_path: &path::Path, output_dir: &path::Path) -> anyhow::Result<()> {
     let file = io::BufReader::new(fs::File::open(file_path)?);
     let img = image::load(file, image::ImageFormat::Png)?;
 
     let buffer = img.to_rgb8();
-    let file_name_base = file_path.file_stem().unwrap();
+    let file_name_base = String::from(file_path.file_stem().unwrap().to_str().unwrap());
     println!("{:?}", file_name_base);
 
     image::save_buffer_with_format(
-        format!("out/{}.jpeg", file_name_base.to_str().unwrap()),
+        format!("{}/{file_name_base}.jpeg", output_dir.to_str().unwrap()),
         &buffer,
         buffer.width(),
         buffer.height(),
@@ -24,17 +24,15 @@ pub fn convert(file_path: &path::Path) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn list_files() -> anyhow::Result<Vec<String>> {
-    let out = fs::read_dir("in")?
-        .filter_map(|dir_entry| {
-            let entry = dir_entry.unwrap();
-            let entry_type = entry.file_type().unwrap();
+pub fn list_files(input_dir: &str) -> anyhow::Result<impl Iterator<Item = String>> {
+    let out = fs::read_dir(input_dir)?.filter_map(|dir_entry| {
+        let entry = dir_entry.unwrap();
+        let entry_type = entry.file_type().unwrap();
 
-            match entry_type.is_file() {
-                true => Some(String::from(entry.path().as_path().to_str().unwrap())),
-                false => None,
-            }
-        })
-        .collect();
+        match entry_type.is_file() {
+            true => Some(String::from(entry.path().as_path().to_str().unwrap())),
+            false => None,
+        }
+    });
     Ok(out)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
+use std::env;
 use std::path;
 
 fn main() -> anyhow::Result<()> {
-    let files = imabacon::list_files()?;
-    for file in files {
-        imabacon::convert(&path::Path::new(&file))?;
+    let (input_dir, output_dir) = args()?;
+    for file in imabacon::list_files(&input_dir)? {
+        imabacon::convert(path::Path::new(&file), path::Path::new(&output_dir))?;
     }
     Ok(())
+}
+
+fn args() -> anyhow::Result<(String, String)> {
+    let mut args = env::args().enumerate();
+
+    args.next().unwrap(); // consume first arg
+
+    let Some((_, input_dir)) = args.next() else {
+        return Err(anyhow::Error::msg("Invalid input argument"));
+    };
+    let Some((_, out_dir)) = args.next() else {
+        return Err(anyhow::Error::msg("Invalid output argument"));
+    };
+
+    Ok((input_dir, out_dir))
 }
